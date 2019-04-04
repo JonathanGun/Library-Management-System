f59b7350d08100a83eba19cb83c979751827b472
JoeGun <39112190+JonathanGun@users.noreply.github.com>
2019-04-04 17:53:36 +0700

    Update uuser.pas

--- code/uuser.pas
+++ code/uuser.pas
@@ -1,11 +1,13 @@
 unit uuser;
 
 interface
-
+{PUBLIC VARIABLE, CONST, ADT}
 const
-	MAXUSER = 1000;
+	USER_MAX 	= 1000;
+	USER_COLUMN	= 5;
 
 type
+	{Definisi ADT User}
 	User = record
 		username	: string;
 		password	: string;
@@ -14,9 +16,22 @@ type
 		isAdmin		: boolean;
 	end;
 
-	tuser = array [1..MAXUSER] of User;
+	{Definisi ADT array of User dan pointernya}
+	{Agar dapat digunakan di program utama dan unit lain}
+	tuser = array [1..USER_MAX] of User;
 	puser = ^tuser;
 
+var
+	userNeff: integer;
+
+{PUBLIC FUNCTIONS, PROCEDURE}
+{ - }
+
 implementation
+{PRIVATE VARIABLE, CONST, ADT}
+{ - }
+
+{FUNGSI dan PROSEDUR}
+{ - }
 
 end.
